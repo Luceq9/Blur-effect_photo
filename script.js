f9d7e8365a3374573d4e3d7ef12a90a707a58897
lucjan <lucjanjaroszewski@gmail.com>
2024-10-08 08:09:06 +0200

    blur effect

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const btn = document.querySelector('.btn');
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-btn.addEventListener('click', () => {
-  console.log('hello');
+let load = 0;
+let int = setInterval(blurring, 30);
 
-  const search = document.querySelector('.search');
-  search.classList.toggle('active');
-});
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+function scale(num, in_min, in_max, out_min, out_max) {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
